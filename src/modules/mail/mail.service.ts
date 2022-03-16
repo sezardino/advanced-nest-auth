db14f883847e4613b27fc0fcf252e9b2aad64f2e
Edward Arechwa <e.arechwa@gmail.com>
2022-03-16 18:05:42 +0100

    fix mail service

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -13,28 +13,27 @@ export class MailService {
     );
   }
 
-  async send(to: string, html: string) {
-    this.instance
-      .post('send', {
-        version: 'v3.1',
-      })
-      .request({
-        Messages: [
-          {
-            From: {
-              Email: this.configService.get('API_URL'),
-              Name: this.configService.get('API_NAME'),
-            },
-            To: [
-              {
-                Email: to,
-                Name: to,
-              },
-            ],
-            HTMLPart: html,
+  send(to: string, html: string, subject: string, text: string) {
+    this.instance.post('send', { version: 'v3.1' }).request({
+      Messages: [
+        {
+          From: {
+            Email: this.configService.get('MAILJET_SENDER'),
+            Name: this.configService.get('API_NAME'),
           },
-        ],
-      });
+          To: [
+            {
+              Email: to,
+              Name: 'to',
+            },
+          ],
+          Subject: subject,
+          TextPart: text,
+          HTMLPart: html,
+          CustomID: Date.now().toString(),
+        },
+      ],
+    });
   }
 
   async sendActivationMail(to: string, link: string) {
@@ -43,6 +42,9 @@ export class MailService {
                   <a href="${link}">${link}</a>
                 `;
 
-    await this.send(to, html);
+    const subject = 'Activation link';
+    const textPart = 'Activation link';
+
+    this.send(to, html, subject, textPart);
   }
 }
